15a4e044709bc5bd9a27b9522d90fe6c3ff3f545
Prasanna Tuladhar <tuladharprasan@gmail.com>
2023-02-20 09:51:06 +0100

    Added styling

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Product, FooterBanner, HeroBanner} from '../components'
+
+const index = () => {
+  return (
+    <div>
+      <HeroBanner/>
+
+      <div className='products-heading'>
+        <h2>Best selling Products</h2>
+        <p>Speakers of many variations</p>
+
+      </div>
+      <div className='products-container'>
+        {['Product 1','Product 2'].map(
+          (product)=> <p>{product}</p>
+        )}
+      </div>
+      <div>
+<FooterBanner/>
+      </div>
+    </div>
+  )
+}
+
+export default index
